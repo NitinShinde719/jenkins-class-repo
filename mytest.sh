fe6815e5ae180383cc74f6e7c9b12831b81294f1
NitinShinde719 <54055131+NitinShinde719@users.noreply.github.com>
2021-04-08 17:12:38 +0530

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -2,4 +2,7 @@ echo "This is test Data"
 date
 cal
 echo " This is a Demo GitHub hook GITScm Polling"
+echo " repo --> setting --> webhhok-->Add Webhook --> give payload url--> API "
+echo " http://Ipaddress:8080/github-webhook"
+
 
